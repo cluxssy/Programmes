3df12304d6fa66075727111a640ce1a7bb315aef
Priyanshu Sharma <cluxssy25@gmail.com>
2023-07-21 17:18:58 +0530

    Add files via upload

---
+++ Age calculator from birth year.py
@@ -0,0 +1,3 @@
+birth_year = input('Type your Birth Year? ')
+age= (2023 - int(birth_year))
+print(f'your age is {age}')
---
+++ Pound to KG weight convertor.py
@@ -0,0 +1,3 @@
+PoundWeight = int(input('What is your weight in pounds? '))
+KgWeight = (PoundWeight/2.205)
+print(f'Your Weight in Kg is: {KgWeight}')
